6029eb066029adcb3ced8cbae5ed48fa80f8ec67
priyasingh876 <85429167+priyasingh876@users.noreply.github.com>
2023-12-14 17:16:36 +0530

    Add files via upload

---
+++ BookNotFoundException.java
@@ -0,0 +1,18 @@
+package pkg.exception;
+
+public class BookNotFoundException extends Exception {
+	
+	
+	private static final long serialVersionUID = 1L;
+
+	public  BookNotFoundException() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "BookNotFoundException is Generated";
+	}
+	
+
+}
