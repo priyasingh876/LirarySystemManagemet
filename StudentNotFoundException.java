db62732b7dd97157852a9e619b1e9cdbd16ccd99
priyasingh876 <85429167+priyasingh876@users.noreply.github.com>
2023-12-14 17:16:56 +0530

    Add files via upload

---
+++ StudentNotFoundException.java
@@ -0,0 +1,26 @@
+package pkg.exception;
+
+
+
+public class StudentNotFoundException extends Exception {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public StudentNotFoundException() {
+		super();
+		
+	}
+
+	@Override
+	public String toString() {
+		return "StudentNotFoundException is Generated";
+	}
+
+	
+
+	
+	
+	
+
+}
